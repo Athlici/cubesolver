5566637d2079e5f0a668a5ac5eb4a623bb362b86
Tobias Stamm <tobias.stamm@wilhelm-gym.net>
2012-05-02 13:51:43 +0200

    modified create.cpp

--- create.cpp
+++ create.cpp
@@ -1,8 +1,72 @@
 void getcenters() {
- centers = (unsigned char*) malloc(centersize);
- if(!LoadFile(centers,"centers",centersize)) {
-   for(int i=0;i<centersize;i++) *(centers+i) = 255;
- }
+  centers = (unsigned char*) malloc(centersize);
+  if(!LoadFile(centers,"centers",centersize)) {
+    *centers=240;
+    for(int i=1;i<centersize;i++) *(centers+i) = 255;
+    unsigned char depth=0;
+    unsigned char* tmpbegin=(unsigned char*) malloc(1000000);
+    unsigned char* tmptmp=tmpbegin;
+    for(int i=0;i<8;i++) { *tmptmp=i; tmptmp++;}
+    unsigned char* tmpend=tmptmp;
+    unsigned char* tmppos=tmptmp;
+    while (tmpend>tmpbegin){
+      tmptmp=tmpbegin;
+      tmppos=tmpend;
+      depth++;
+      while (tmptmp<tmppos){
+	for (int i=0;i<36;i++) {
+	  unsigned char a=centermove[i][*tmptmp];
+	  unsigned char b=centermove[i][*(tmptmp+1)];
+          unsigned char c=centermove[i][*(tmptmp+2)];
+          unsigned char d=centermove[i][*(tmptmp+3)];
+          unsigned char e=centermove[i][*(tmptmp+4)];
+          unsigned char f=centermove[i][*(tmptmp+5)];
+          unsigned char g=centermove[i][*(tmptmp+6)];
+	  unsigned char h=centermove[i][*(tmptmp+7)];
+	  tmptmp+=8;
+	  int i=poscenters(a,b,c,d,e,f,g,h);
+	  if (depth<readhalfbyte(*(centers+i),i%2)){
+	    *(centers+h)=sethalfbyte(*(centers+i),depth,i%2);
+	    *tmpend=a;
+	    *(tmpend+1)=b;
+            *(tmpend+2)=c;
+            *(tmpend+3)=d;
+            *(tmpend+4)=e;
+            *(tmpend+5)=f;
+            *(tmpend+6)=g;
+	    *(tmpend+7)=h;
+	    tmpend+=8;
+	  }
+	}
+      }
+      if (tmpend-tmppos>tmppos-tmpbegin){
+	for (int i=tmppos-tmpbegin;i>6;i-=8){			//>= oder > ???
+	  *(tmppos-i)=*(tmpend-i);
+	  *(tmppos-i+1)=*(tmpend-i+1);
+          *(tmppos-i+2)=*(tmpend-i+2);
+          *(tmppos-i+3)=*(tmpend-i+3);
+          *(tmppos-i+4)=*(tmpend-i+4);
+          *(tmppos-i+5)=*(tmpend-i+5);
+          *(tmppos-i+6)=*(tmpend-i+6);
+	  *(tmppos-i+7)=*(tmpend-i+7);
+	}
+	tmpend=tmpend-tmppos+tmpbegin;
+      }
+      else{
+	for (int i=tmpend-tmppos;i>6;i-=8){
+	  *(tmpbegin+i)=*(tmppos+i);
+	  *(tmpbegin+i-1)=*(tmppos+i-1);
+          *(tmpbegin+i-2)=*(tmppos+i-2);
+          *(tmpbegin+i-3)=*(tmppos+i-3);
+          *(tmpbegin+i-4)=*(tmppos+i-4);
+          *(tmpbegin+i-5)=*(tmppos+i-5);
+          *(tmpbegin+i-6)=*(tmppos+i-6);
+	  *(tmpbegin+i-7)=*(tmppos+i-7);
+	}
+	tmpend=tmpbegin+tmpend-tmppos;
+      }
+    }
+  }
 }
 
 void getedges() {
@@ -13,9 +77,7 @@ void getedges() {
     unsigned char depth=0;
     unsigned char* tmpbegin=(unsigned char*) malloc(1000000);
     unsigned char* tmptmp=tmpbegin;
-    for(int i=0;i<7;i++) {
-      *tmptmp= 3i;
-      tmptmp++;}
+    for(int i=0;i<7;i++) {*tmptmp= 3i; tmptmp++;}
     unsigned char* tmpend=tmptmp;
     unsigned char* tmppos=tmptmp;
     while (tmpend>tmpbegin){
