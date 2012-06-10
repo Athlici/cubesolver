8878ad39df6a1d32125d211338ec5505b31b69f1
Tobias Stamm <tobias.stamm@wilhelm-gym.net>
2012-06-10 13:35:36 +0200

    minor changes/bugfixes

--- solve.cpp
+++ solve.cpp
@@ -1,12 +1,12 @@
 unsigned char* solve(unsigned char* Position){
   unsigned char* Posbegin=Position;
   unsigned char* Postmp=Position;
-  Position+=55;
   *(Position+55)=36;
+  Position+=56;
   unsigned char* Posend=Position;
   unsigned char Depth=0;
   unsigned char min=255;
-  for (unsigned char i=0;i<23;i++){
+  for (unsigned char i=0;i<22;i++){
     turncube(Postmp,Posend,i);
     *(Posend+55)=37+i;
     Posend+=56;
@@ -41,24 +41,25 @@ unsigned char* solve(unsigned char* Position){
       }
     }
     Postmp=Posend;
-    for (unsigned char* i=Position;i<Postmp;i+=55+Depth){
-      unsigned char tmp=*(edges+posedges(*i,*(i+1),*(i+2),*(i+3),*(i+4),*(i+5),*(i+6)))+
+    for (unsigned char* i=Position;i<Postmp;i+=56+Depth){
+      unsigned char tmp=42;
+/*(edges+posedges(*i,*(i+1),*(i+2),*(i+3),*(i+4),*(i+5),*(i+6)))+
 //        *(corners+poscorners(*(i+),*(i+),*(i+),*(i+),*(i+),*(i+)))+
 //        *(corners+poscorners(*(i+),*(i+),*(i+),*(i+),*(i+),*(i+)))+
 //        *(corners+poscorners(*(i+),*(i+),*(i+),*(i+),*(i+),*(i+)))+
 //        *(corners+poscorners(*(i+),*(i+),*(i+),*(i+),*(i+),*(i+)))+
         *(centers+poscenters(*(i+31),*(i+32),*(i+33),*(i+34),*(i+39),*(i+40),*(i+41),*(i+42)))+
         *(centers+poscenters(*(i+35),*(i+36),*(i+37),*(i+38),*(i+43),*(i+44),*(i+45),*(i+46)))+
-        *(centers+poscenters(*(i+47),*(i+48),*(i+49),*(i+50),*(i+51),*(i+52),*(i+53),*(i+54)));
+        *(centers+poscenters(*(i+47),*(i+48),*(i+49),*(i+50),*(i+51),*(i+52),*(i+53),*(i+54)));*/
       if(tmp<min){
 	min=tmp;
 	Posend=Postmp;
-	for (unsigned char j=0;j<55+Depth;j++) *(Posend+j)=*(i+j);
-	Posend+=55+Depth;
+	for (unsigned char j=0;j<56+Depth;j++) *(Posend+j)=*(i+j);
+	Posend+=56+Depth;
       }
       else if(tmp==min){
-        for (unsigned char j=0;j<55+Depth;j++) *(Posend+j)=*(i+j);
-	Posend+=55+Depth; 
+        for (unsigned char j=0;j<56+Depth;j++) *(Posend+j)=*(i+j);
+	Posend+=56+Depth; 
 	}
     }
     for (int i=0;i<(Posend-Postmp);i+=1) *(Posbegin+i)=*(Postmp+i);
