f76a217e420c22dd498c61edf03c5abfd27dfa14
Tobias Stamm <tobias.stammwilhelm-gym.net>
2014-12-22 01:15:26 +0100

    mayor bugfix + random move generator

--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@ const char*    tablename[3] = {"edge","center","corner"};
 const uint8_t  elemsize [3] = {7,8,6};
 const uint8_t  elemsol[3][8]= {{0,3,6,9,12,15,18,0},{0,1,2,3,8,9,10,11},{0,1,2,3,4,5,0,0}};
 
-uint8_t solution[10];
+uint8_t solution[20];
 
 #include "arrays.cpp"					//file with all the arrays for making the turns
 #include "helpers.cpp"					//various helper files for read, write and movemaking
@@ -42,15 +42,18 @@ gentable(0);
 gentable(1);
 gentable(2);
 
-uint8_t n = 10;
+uint8_t n = 15;
 
 cube Cube=goal();
-uint8_t moves[10]={12, 18, 28, 9, 11, 14, 3, 33, 15, 1};
+uint8_t moves[15]={29, 3, 33, 21, 10, 34, 1, 35, 11, 25, 16, 5, 35, 16, 0};
 for(uint8_t i=0;i<n;i++) Cube=movecube(Cube,moves[i]);
 //printcube(Cube);
 
-for(uint8_t i=0;i<10&&(!solve(Cube,i));i++);
+cout << minDepth(Cube)+0 << "\n";
+
+for(uint8_t i=minDepth(Cube);i<=n&&(!solve(Cube,i));i++)
+  cout << "finished depth " << i+0 << "\n";
 //solve(Cube,7);
-for(uint8_t i=0;i<10;i++)cout << solution[i]+0 << ";"; cout << "\n";
+for(uint8_t i=0;i<n;i++)cout << solution[i]+0 << ";"; cout << "\n";
 
 }
