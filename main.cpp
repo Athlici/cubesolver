37f6ab8a5db46ab3a2d992afa84ac0283bed0f6a
Tobias Stamm <tobias.stamm@wilhelm-gym.net>
2015-10-18 18:30:08 +0200

    added parallel solving

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
-#include <thread>
+#include <future>
 #include <stdint.h>
 #include <string.h>
 #include <math.h>
@@ -37,7 +37,7 @@ const char* tablename[3] = {"center","corner","edge"};
 //const uint8_t  elemsize [3] = {7,8,6};
 //const uint8_t  elemsol[3][8]= {{0,3,6,9,12,15,18,0},{0,1,2,3,8,9,10,11},{0,1,2,3,4,5,0,0}};
 
-uint8_t solution[20];
+uint8_t solution[36][20];
 
 #include "arrays.cpp"					//file with all the arrays for making the turns
 #include "helpers.cpp"					//various helper files for read, write and movemaking
@@ -48,13 +48,6 @@ uint8_t solution[20];
 #include "solve.cpp"					//solves a given cube position optimal
 
 int main(int argc, char** argv) {
-
-//thread t1(gentable[0]);					//parallel creation and loading of the tables
-//thread t2(gentable[1]);
-//thread t3(gentable[2]); 
-//t1.join();
-//t2.join();
-//t3.join();
 
 gentable(0);
 gentable(1);
@@ -75,9 +68,8 @@ printcube(Cube);
 
 cout << minDepth(Cube)+0 << "\n";
 
-for(uint8_t i=minDepth(Cube);i<=n&&(!solve(Cube,i));i++)
+for(uint8_t i=minDepth(Cube);i<=n&&(!solvepar(Cube,i));i++)
   cout << "finished depth " << i+0 << "\n";
 //solve(Cube,7);
-for(uint8_t i=0;i<n;i++)cout << solution[i]+0 << ";"; cout << "\n";
 
 }
--- solve.cpp
+++ solve.cpp
@@ -1,15 +1,35 @@
-bool solve(cube Cube, uint8_t rdepth){
+bool solve(cube Cube, uint8_t rdepth, uint8_t t){
   if(rdepth>0){
     if(rdepth>=minDepth(Cube)){
       for(uint8_t move=0;move<36;move++){
         cube nCube=movecube(Cube,move);
-	if(solve(nCube,rdepth-1)){
-	  solution[rdepth-1]=move;
-	  return true;
-	}
+    if(solve(nCube,rdepth-1,t)){
+      solution[t][rdepth-1]=move;
+      return true;
+    }
       }
     }
     return false;
   }else
     return solved(Cube);
+}
+
+bool solvepar(cube Cube, uint8_t rdepth){
+  future<bool> par[36];
+  for(uint8_t move=0;move<36;move++){
+    solution[move][rdepth-1]=move;
+    cube nCube=movecube(Cube,move);
+    par[move] =async(launch::async,solve,nCube,rdepth-1,move);
+  }
+  for(uint8_t i=0;i<36;i++)
+    par[i].wait();
+  bool res=false;
+  for(uint8_t i=0;i<36;i++)
+    if(par[i].get()){
+      res=true;
+      for(uint8_t j=0;j<rdepth;j++)
+        cout << solution[i][j]+0 << ";";
+      cout << "\n";
+    }
+  return res;
 }
